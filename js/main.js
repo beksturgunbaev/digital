5915861603b3d4a528282d6c53d6bb744c7202e3
beksultan <turgunbaaev@gmail.com>
2021-08-02 21:01:12 +0600

    Finished desktop version

---
+++ js/main.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+    $('.cases_slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        draggable: true,
+        infinity: true,
+        dots: true
+    });
+});
+
+// Accordion:
+const acc = document.getElementsByClassName("acc_btn");
+
+for (let i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        let panel = this.nextElementSibling;
+        let icon = this.querySelector('span');
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+            icon.innerText = '+';
+
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+            icon.innerText = '-';
+        }
+    });
+}
